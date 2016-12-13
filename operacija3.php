1148a0ed0a6d95d99847be074910e6653ee76cfe
Leocanis <kuzminskast@gmail.com>
2016-12-13 19:56:09 +0200

    bandau commit

---
+++ operacija3.php
@@ -0,0 +1,49 @@
+<?php
+include("include/session.php");
+//Jei prisijunges Administratorius ar Valdytojas vykdomas operacija3 kodas
+if ($session->logged_in && ($session->isAdmin() || $session->isManager())) {
+    ?>    
+    <html>  
+        <head>  
+            <meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8"/> 
+            <title>SLC ĮS</title>
+			<!-- jQuery library -->
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+			<!-- Latest compiled JavaScript -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			
+        </head>
+        <body>       
+            <table class="center"><tr><td>
+                        <img src="pictures/top.png"/>
+                    </td></tr><tr><td> 
+                        <?php
+                        include("include/meniu.php");
+                        ?>                   
+                        <table style="border-width: 2px; border-style: dotted;"><tr><td>
+                                    Atgal į [<a href="index.php">Pradžia</a>]
+                                </td></tr></table>               
+                        <br> 
+                        <div style="text-align: center;color:green">                   
+                            <h1>Operacija 3.</h1>
+                            Vėl rodomas meniu ir nuoroda į pradžią.                   
+                        </div> 
+                        <br>                         
+                <tr><td>
+                        <?php
+                        include("include/footer.php");
+                        ?>
+                    </td></tr>                       
+            </table>     
+        </body>
+    </html>
+    <?php
+    //Jei vartotojas neprisijungęs arba prisijunges, bet ne Administratorius 
+    //ar ne Valdytojas - užkraunamas pradinis puslapis   
+} else {
+    header("Location: index.php");
+}
+?>
+
